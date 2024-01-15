c324c2fa3b105f4ac1c86242bd56b8263b228100
PaoloCaldera <paolo.caldera@outlook.com>
2024-01-15 15:50:01 +0100

    feature: refine layout and improve the user interface
    Improve the user interface of the composable layout. Specifically, introduce spacings between the image and the text in the artwork presentation, then put the buttons to the bottom. Add then a surface around the picture.
    Shift the logic for composing the artwork in the ArtworkPresentation composable.
    Remove the id variable inside the Artwork data class.
    
    Integrates #1

--- app/src/main/java/com/example/artspace/Artwork.kt
+++ app/src/main/java/com/example/artspace/Artwork.kt
@@ -3,7 +3,6 @@ package com.example.artspace
 import androidx.compose.ui.graphics.painter.Painter
 
 data class Artwork(
-    val id: Int,
     val image: Painter,
     val contentDescription: String,
     val title: String,
--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -4,9 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -16,10 +23,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,11 +55,49 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ComposeMainActivity(modifier: Modifier = Modifier) {
-    var counter by remember { mutableStateOf(1) }
+    var counter by remember { mutableStateOf(4) }
+
+
+
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(32.dp),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            modifier = modifier.weight(5f),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ArtworkPresentation(counter = counter)
+        }
+
+        Row(
+            modifier = modifier
+                .weight(1f)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.width(128.dp)) {
+                Text(text = stringResource(R.string.previous_button))
+            }
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.width(128.dp)) {
+                Text(text = stringResource(R.string.next_button))
+            }
+        }
+    }
+
+}
+
+
+@Composable
+fun ArtworkPresentation(counter: Int, modifier: Modifier = Modifier) {
 
     val artwork = when (counter) {
         1 -> Artwork(
-            id = 1,
             image = painterResource(R.drawable.monna_lisa),
             contentDescription = stringResource(R.string.content_description_1),
             title = stringResource(R.string.title_1),
@@ -54,7 +106,6 @@ fun ComposeMainActivity(modifier: Modifier = Modifier) {
         )
 
         2 -> Artwork(
-            id = 2,
             image = painterResource(R.drawable.cenacolo),
             contentDescription = stringResource(R.string.content_description_2),
             title = stringResource(R.string.title_2),
@@ -63,7 +114,6 @@ fun ComposeMainActivity(modifier: Modifier = Modifier) {
         )
 
         3 -> Artwork(
-            id = 3,
             image = painterResource(R.drawable.primavera),
             contentDescription = stringResource(R.string.content_description_3),
             title = stringResource(R.string.title_3),
@@ -72,7 +122,6 @@ fun ComposeMainActivity(modifier: Modifier = Modifier) {
         )
 
         4 -> Artwork(
-            id = 4,
             image = painterResource(R.drawable.le_moulin_de_la_galette),
             contentDescription = stringResource(R.string.content_description_4),
             title = stringResource(R.string.title_4),
@@ -81,7 +130,6 @@ fun ComposeMainActivity(modifier: Modifier = Modifier) {
         )
 
         5 -> Artwork(
-            id = 5,
             image = painterResource(R.drawable.notte_stellata),
             contentDescription = stringResource(R.string.content_description_5),
             title = stringResource(R.string.title_5),
@@ -92,27 +140,32 @@ fun ComposeMainActivity(modifier: Modifier = Modifier) {
         else -> throw Exception("Invalid counter value. Allowed values are from 1 to 5.")
     }
 
-    Column {
-        ArtworkPresentation(artwork = artwork)
-
-        Row {
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = stringResource(R.string.previous_button))
-            }
-            Button(onClick = { /*TODO*/ }) {
-                Text(text = stringResource(R.string.next_button))
-            }
-        }
+    Surface(shadowElevation = 32.dp) {
+        Image(
+            painter = artwork.image,
+            contentDescription = artwork.contentDescription,
+            contentScale = ContentScale.Fit,
+            modifier = modifier
+                .padding(32.dp)
+                .aspectRatio(artwork.image.intrinsicSize.width / artwork.image.intrinsicSize.height)
+                .fillMaxWidth()
+        )
     }
 
-}
-
-
-@Composable
-fun ArtworkPresentation(artwork: Artwork, modifier: Modifier = Modifier) {
-    Image(painter = artwork.image, contentDescription = artwork.contentDescription)
-    Text(text = artwork.title)
-    Text(text = stringResource(R.string.artist_and_year, artwork.artist, artwork.year))
+    Spacer(modifier = Modifier.size(32.dp))
+    Text(
+        text = artwork.title,
+        fontFamily = FontFamily.Serif,
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center
+    )
+    Text(
+        text = stringResource(R.string.artist_and_year, artwork.artist, artwork.year),
+        fontFamily = FontFamily.Serif,
+        fontSize = 16.sp,
+        textAlign = TextAlign.Center
+    )
 }
 
 
