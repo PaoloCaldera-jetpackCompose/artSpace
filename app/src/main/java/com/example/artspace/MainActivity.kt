ecbcc206c5fb81d57e5f2d1042900504bd2a60ad
PaoloCaldera <paolo.caldera@outlook.com>
2024-01-15 14:50:09 +0100

    feature: initialize Artwork object in composable
    Define which Artwork object will be used based on the counter value.
    The variable counter is made observable and stateful in order the app to have property on which image to display everytime.
    
    Integrates #1

--- app/src/main/java/com/example/artspace/MainActivity.kt
+++ app/src/main/java/com/example/artspace/MainActivity.kt
@@ -7,8 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.artspace.ui.theme.ArtSpaceTheme
 
@@ -31,9 +36,58 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ComposeMainActivity(modifier: Modifier = Modifier) {
+    var counter by remember { mutableStateOf(1) }
+
+    val artwork = when (counter) {
+        1 -> Artwork(
+            id = 1,
+            image = painterResource(R.drawable.monna_lisa),
+            contentDescription = stringResource(R.string.content_description_1),
+            title = stringResource(R.string.title_1),
+            artist = stringResource(R.string.artist_1),
+            year = stringResource(R.string.year_1)
+        )
+
+        2 -> Artwork(
+            id = 2,
+            image = painterResource(R.drawable.cenacolo),
+            contentDescription = stringResource(R.string.content_description_2),
+            title = stringResource(R.string.title_2),
+            artist = stringResource(R.string.artist_2),
+            year = stringResource(R.string.year_2)
+        )
+
+        3 -> Artwork(
+            id = 3,
+            image = painterResource(R.drawable.primavera),
+            contentDescription = stringResource(R.string.content_description_3),
+            title = stringResource(R.string.title_3),
+            artist = stringResource(R.string.artist_3),
+            year = stringResource(R.string.year_3)
+        )
+
+        4 -> Artwork(
+            id = 4,
+            image = painterResource(R.drawable.le_moulin_de_la_galette),
+            contentDescription = stringResource(R.string.content_description_4),
+            title = stringResource(R.string.title_4),
+            artist = stringResource(R.string.artist_4),
+            year = stringResource(R.string.year_4)
+        )
+
+        5 -> Artwork(
+            id = 5,
+            image = painterResource(R.drawable.notte_stellata),
+            contentDescription = stringResource(R.string.content_description_5),
+            title = stringResource(R.string.title_5),
+            artist = stringResource(R.string.artist_5),
+            year = stringResource(R.string.year_5)
+        )
+
+        else -> throw Exception("Invalid counter value. Allowed values are from 1 to 5.")
+    }
 
 }
-
 
 
 @Preview(showBackground = true)
